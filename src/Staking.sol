20e9290a51ab596d687330b444c4d8efd0e79974
AlanDXiang <dongxuxiang1024@gmail.com>
2026-02-10 19:44:13 +0800

    debug: adding ReentrancyGuard.sol

--- src/Staking.sol
+++ src/Staking.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.20;
 // We use OpenZeppelin for security. In Remix, these imports work automatically.
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract StakingPool is ReentrancyGuard, Ownable {
